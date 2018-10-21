31310f2290c3b073154ff1899ff3b836d22662fa
rick <test@gmail.com>
2018-10-21 17:20:51 +0900

    Fleshed out client details component

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+
+import { Client } from '../../models/Client';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-client-details',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientDetailsComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  client: Client;
+  hasBalance = false;
+  showBalanceUpdateInput = false;
+
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+    // Get id from url
+    this.id = this.route.snapshot.params['id'];
+    // Get client
+    this.clientService.getClient(this.id).subscribe(client => {
+      if (client != null) {
+        if (client.balance > 0) {
+          this.hasBalance = true;
+        }
+      }
+      this.client = client;
+    });
   }
-
 }
