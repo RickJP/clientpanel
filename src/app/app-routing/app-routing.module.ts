e7c0fd31fc5c51b2661b025c06160c4bdcb9d778
Rick <3741326+RickJP@users.noreply.github.com>
2018-10-21 20:33:36 +0900

    Implemented functionality for editing and deleting of clients

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -16,8 +16,7 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'client/add', component: AddClientComponent},
-  {path: 'client/edit/:id', component: DashboardComponent},
-
+  {path: 'client/edit/:id', component: EditClientComponent},
   {path: 'client/:id', component: ClientDetailsComponent},
   {path: 'settings', component: SettingsComponent},
   {path: '**', component: NotFoundComponent},
